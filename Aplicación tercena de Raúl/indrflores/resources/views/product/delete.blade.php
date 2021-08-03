1b30f201e7f82127b1ab5b9b1dd342eff496b26c
wellinmart32 <wellinmart32@gmail.com>
2021-08-03 11:24:56 -0500

    Añadiendo indrflores

---
+++ Aplicación tercena de Raúl/indrflores/resources/views/product/delete.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="panel panel-heading">
+                <div class="row">
+                    Agregar producto
+                    <a href="/products" class="btn btn-primary btn-sm">Volver a productos</a>
+                </div>
+            </div>
+
+            <div class="panel-body">
+                @if ( session('mensaje') )
+                    <div class="alert alert-success">{{ session('mensaje') }}</div>
+                @endif
+
+                <form method="POST" action="{{ action('ProductController@destroy', $product->id) }}">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+
+                    <input
+                      type="text"
+                      name="code"
+                      placeholder="Código"
+                      value="{{ $product->code }}"
+                      class="form-control mb-2"
+                      disabled="disabled"
+                    />
+
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Nombre"
+                      value="{{ $product->name }}"
+                      class="form-control mb-2"
+                      disabled="disabled"
+                    />
+
+                    <input
+                      type="text"
+                      name="description"
+                      placeholder="Descripcion"
+                      value="{{ $product->description }}"
+                      class="form-control mb-2"
+                      disabled="disabled"
+                    />
+
+                    <input
+                      type="text"
+                      name="weight"
+                      placeholder="Peso"
+                      value="{{ $product->weight }}"
+                      class="form-control mb-2"
+                      disabled="disabled"
+                    />
+
+                    <input
+                      type="text"
+                      name="state"
+                      placeholder="Estado"
+                      value="{{ $product->state }}"
+                      class="form-control mb-2"
+                      disabled="disabled"
+                    />
+
+                    <button class="btn btn-danger btn-block" type="submit">Eliminar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
