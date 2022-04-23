b47a929ba1805d3ff3117f7849a8a4df98a7553a
wellinmart32 <wellinmart32@gmail.com>
2022-04-22 20:28:05 -0500

    Actualizando ultimos proyectos
    Actualizando los proyectos realizados hasta el 22/04/2022

---
+++ academicnotes/resources/views/poo.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <h1>Los diagramas de flujo, Tipos de diagramas de flujo y su Simbología, estándares</h1>
+        <h2>
+            Los diagramas de flujo son una representación gráfica del algoritmo, nos permite observa, el flujo del proceso, a través de la representación gráfica de los pasos del algortimo, es la herramienta más utilizadas de un programador o ingeniero en sistemas para diseñar algoritmos (Joyanes Aguilar, 2019):
+            A continuación, vemos la equivalencia de un algortimo, y su representación en Flujograma.
+        </h2>
+    </body>
+</html>
---
+++ academicnotes/resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <h1>Los diagramas de flujo, Tipos de diagramas de flujo y su Simbología, estándares</h1>
+        <p>
+            Los diagramas de flujo son una representación gráfica del algoritmo, nos permite observa, el flujo del proceso, a través de la representación gráfica de los pasos del algortimo, es la herramienta más utilizadas de un programador o ingeniero en sistemas para diseñar algoritmos (Joyanes Aguilar, 2019):
+            A continuación, vemos la equivalencia de un algortimo, y su representación en Flujograma.
+        </p>
+        <p>
+        Ahora empecemos a construir el flujograma, aplicando las siguientes reglas:
+            <br>
+            1. Todo flujograma inicia y termina con el símbolo
+            Dentro de ella colocamos, la palabra INICIO O FIN para representar los dos extremos que limitan la estructura de algoritmo
+            <br>
+            2. El flujograma debe de crecer de abajo hacia arriba, y de izquierda a derecha, y los símbolos del flujo son unidos por los conectores de línea, con su respectiva punta de flecha, que representa la dirección del flujo.
+            <br>
+            3. Luego debemos declarar las variables que son usadas en el algoritmo, aplicando el símbolo de preparación de datos:
+            <br>
+            4. Las entradas y salidas aplican sus símbolos correspondientes,
+            <br>
+            5. Así mismo el símbolo de procesamiento
+            <br>
+            6. Si el flujo no se logra dibujar en toda la hoja, se puede continuar en un espacio libre, usando conectores en la misma hoja, identificándolos con una letra, en donde termina y en donde continua:
+            <br>
+            7. De la misma manera, si el flujo requiere ser dibujado en otra hoja, usamos un conector de hoja, y lo identificamos con una letra, en donde termina y en donde continua:
+            <br>
+        </p>
+    </body>
+</html>
+
